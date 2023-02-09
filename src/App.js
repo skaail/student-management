05bc899268cd4b0b93d7f6d8c9203a37d02f5609
Bruno <bruno.gianini@readmore.com.br>
2023-02-09 13:04:44 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
+import Salas from './pages/Salas';
+import Solici from './pages/Solici';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+        <Navbar></Navbar>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />}/>
+            <Route path='/cadastro' element={<Cadastro />}/>
+            <Route path='/salas' element={<Salas />}/>
+            <Route path='/solici' element={<Solici />}/>
+          </Routes>
+        </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Salas.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Salas() {
+  return (
+    <div>Salas</div>
+  )
+}
+
+export default Salas
